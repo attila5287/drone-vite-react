889cace7c6437de9c24646be9a3d021757404745
AttilaTurkoz <42498753+attila5287@users.noreply.github.com>
2025-06-20 14:06:20 -0600

    typescript fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,71 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 
 import "mapbox-gl/dist/mapbox-gl.css";
-
 import "./App.css";
+
+const INITIAL_CENTER: [number, number] = [-104.98887493053121, 39.73899257929499]; // civic
+
+const INITIAL_ZOOM = 18.00;
 
 function App() {
   const mapRef = useRef<mapboxgl.Map | null>(null);
-  const mapContainerRef = useRef<HTMLDivElement | null>(null);
+  const mapContainerRef = useRef<HTMLDivElement>(null);
+
+  const [center, setCenter] = useState<[number, number]>(INITIAL_CENTER);
+  const [zoom, setZoom] = useState(INITIAL_ZOOM);
 
   useEffect(() => {
+    if (!mapContainerRef.current) return;
+
     mapboxgl.accessToken =
       "pk.eyJ1IjoiYXR0aWxhNTIiLCJhIjoiY2thOTE3N3l0MDZmczJxcjl6dzZoNDJsbiJ9.bzXjw1xzQcsIhjB_YoAuEw";
     mapRef.current = new mapboxgl.Map({
-      container: mapContainerRef.current as HTMLElement,
-      center: [-104.98887493053121, 39.73899257929499], // civic
-      zoom: 18,
-      pitch: 45,
-      bearing: -40,
-      style: "mapbox://styles/mapbox/standard",
+      container: mapContainerRef.current,
+      center: center,
+      zoom: zoom,
+      pitch: 50,
+      bearing: 30,
+    });
+
+    mapRef.current.on("move", () => {
+      if (!mapRef.current) return;
+      // get the current center coordinates and zoom level from the map
+      const mapCenter = mapRef.current.getCenter();
+      const mapZoom = mapRef.current.getZoom();
+
+      // update state
+      setCenter([mapCenter.lng, mapCenter.lat]);
+      setZoom(mapZoom);
     });
 
     return () => {
-      mapRef.current?.remove();
+      if (mapRef.current) {
+        mapRef.current.remove();
+      }
     };
   }, []);
 
+  const handleButtonClick = () => {
+    if (mapRef.current) {
+      mapRef.current.flyTo({
+        center: INITIAL_CENTER,
+        zoom: INITIAL_ZOOM,
+      });
+    }
+  };
+
   return (
     <>
+      <div className="sidebar">
+        Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} |
+        Zoom: {zoom.toFixed(2)}
+      </div>
+
+      <button className="reset-button" onClick={handleButtonClick}>
+        Reset
+      </button>
+
       <div id="map-container" ref={mapContainerRef} />
     </>
   );
