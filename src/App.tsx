43867a386bb167d4fdffdb29c3b4e065b735fff2
AttilaTurkoz <42498753+attila5287@users.noreply.github.com>
2025-06-20 13:27:28 -0600

    add mb token

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+
+import "mapbox-gl/dist/mapbox-gl.css";
+
 import "./App.css";
 
 function App() {
+  const mapRef = useRef();
+  const mapContainerRef = useRef();
+
+  useEffect(() => {
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoiYXR0aWxhNTIiLCJhIjoiY2thOTE3N3l0MDZmczJxcjl6dzZoNDJsbiJ9.bzXjw1xzQcsIhjB_YoAuEw";
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+    });
+
+    return () => {
+      mapRef.current.remove();
+    };
+  }, []);
+
   return (
     <>
-      <div id="map-container" />
+      <div id="map-container" ref={mapContainerRef} />
     </>
   );
 }
